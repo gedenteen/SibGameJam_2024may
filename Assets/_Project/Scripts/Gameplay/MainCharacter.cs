76c1f4a7270cd3291f2dcd1af8c191695759d43d
gedenteen <Razumow.dmitrij@yandex.ru>
2024-05-08 22:16:23 +0300

    Create main character

---
+++ Assets/_Project/Scripts/Gameplay/MainCharacter.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharacter : MonoBehaviour
+{
+    public float speed;
+
+    [SerializeField] private Rigidbody2D rigidbody2D;
+    
+    private Vector2 moveInput;
+    private Vector2 moveVelocity;
+
+    private void Update()
+    {
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * speed;    
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody2D.MovePosition(rigidbody2D.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
