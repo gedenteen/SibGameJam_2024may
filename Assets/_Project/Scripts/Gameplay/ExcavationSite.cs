e46fa80cad9b3f295880b53477ef42c40e9fd94c
gedenteen <Razumow.dmitrij@yandex.ru>
2024-05-09 20:20:31 +0300

    Implement digging

---
+++ Assets/_Project/Scripts/Gameplay/ExcavationSite.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExcavationSite : MonoBehaviour
+{
+    [SerializeField] private float initialTime = 15f;
+    [SerializeField] private bool isDigging = false;
+    [SerializeField] private List<Sprite> sprites= new List<Sprite>();
+    [SerializeField] private SpriteRenderer targetSpriteRenderer;
+    [SerializeField] private float remainingTime;
+    [SerializeField] private float[] timers;
+
+    private void Awake()
+    {
+        remainingTime = initialTime;
+
+        int countOfSprites = sprites.Count;
+        timers = new float[countOfSprites];
+        for (int i = 0; i < countOfSprites; i++)
+        {
+            timers[i] = (countOfSprites - i - 1) * initialTime / countOfSprites;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (remainingTime <= 0 || !isDigging)
+        {
+            return;
+        }
+
+        Debug.Log($"ExcavationSite: меня копают");
+
+        remainingTime -= Time.fixedDeltaTime;
+        TryChangeSprite();
+    }
+
+    private void TryChangeSprite()
+    {
+        for (int i = 0; i < timers.Length; i++)
+        {
+            if (remainingTime >= timers[i])
+            {
+                targetSpriteRenderer.sprite = sprites[i];
+                break;
+            }
+        }
+    }
+
+    public void SetStateIsDigging(bool value)
+    {
+        isDigging = value;
+    }
+}
--- Assets/_Project/Scripts/Gameplay/MainCharacter.cs
+++ Assets/_Project/Scripts/Gameplay/MainCharacter.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class MainCharacter : Character
 {
     [SerializeField] private Rigidbody2D rigidbody2D;
+    [SerializeField] private GameObject myShovel;
     
     private Vector2 moveInput;
     private Vector2 moveVelocity;
     private bool eventGameOverIsInvoked = false;
+    private bool onExcavationSite = false;
+    private ExcavationSite nearbyExcavationSite = null;
 
     private void Update()
     {
@@ -20,10 +23,58 @@ public class MainCharacter : Character
             GameplayEvents.eventGameOver.Invoke();
             eventGameOverIsInvoked = true;
         } 
+
+        if (nearbyExcavationSite != null)
+        {
+            if (onExcavationSite && Input.GetKey(KeyCode.E))
+            {
+                Dig(true);
+            }
+            else
+            {
+                Dig(false);
+            }
+        }
     }
 
     private void FixedUpdate()
     {
         rigidbody2D.MovePosition(rigidbody2D.position + moveVelocity * Time.fixedDeltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (CheckOnExcavationSite(other))
+        {
+            onExcavationSite = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (CheckOnExcavationSite(other))
+        {
+            onExcavationSite = false;
+        }
+    }
+
+    private bool CheckOnExcavationSite(Collider2D other)
+    {
+        ExcavationSite excavationSite = null;
+        other.TryGetComponent<ExcavationSite>(out excavationSite);
+        
+        if (excavationSite != null)
+        {
+            nearbyExcavationSite = excavationSite;
+            return true;
+        }
+
+        return false;
+    }
+
+    private void Dig(bool value)
+    {
+        myShovel.SetActive(value);
+        nearbyExcavationSite.SetStateIsDigging(value);
     }
 }
