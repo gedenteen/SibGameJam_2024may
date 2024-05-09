135b790684363bae9c599c6b6ea8ddfc4075ca06
gedenteen <Razumow.dmitrij@yandex.ru>
2024-05-09 19:04:40 +0300

    Implement animation for zombies

--- Assets/_Project/Scripts/Gameplay/Enemy.cs
+++ Assets/_Project/Scripts/Gameplay/Enemy.cs
@@ -8,6 +8,8 @@ public class Enemy : MonoBehaviour
     public float speed = 3f;
     public float hitpoints = 2;
 
+    private bool facingRight = true;
+
     private MainCharacter mainCharacter;
 
     private void Start()
@@ -22,6 +24,33 @@ public class Enemy : MonoBehaviour
 
     private void Move()
     {
+        Vector3 oldPosition = transform.position;
         transform.position = Vector2.MoveTowards(transform.position, mainCharacter.transform.position, speed * Time.fixedDeltaTime);
+        Vector3 offset = transform.position - oldPosition;
+
+        if (!facingRight && offset.x > 0)
+        {
+            Flip();
+        }
+        else if (facingRight && offset.x < 0)
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        
+        Vector3 eulerAngles = transform.eulerAngles;
+        if (facingRight)
+        {
+            eulerAngles.y = 0f;
+        }
+        else
+        {
+            eulerAngles.y = 180f;
+        }
+        transform.eulerAngles = eulerAngles;
     }
 }
