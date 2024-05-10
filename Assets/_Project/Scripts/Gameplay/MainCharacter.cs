cc9e8e6da5e58078478dabe427652abaa4c2bbb9
D00mskey <64736179+D00mskey@users.noreply.github.com>
2024-05-10 05:49:29 +0500

    make attack

--- Assets/_Project/Scripts/Gameplay/MainCharacter.cs
+++ Assets/_Project/Scripts/Gameplay/MainCharacter.cs
@@ -6,13 +6,20 @@ public class MainCharacter : MonoBehaviour
 {
     public float speed = 7f;
     public int hitpoints = 5;
+    public int damage = 1;
 
     [SerializeField] private Rigidbody2D rigidbody2D;
     
     private Vector2 moveInput;
     private Vector2 moveVelocity;
     private bool eventGameOverIsInvoked = false;
+    private WeaponParent weaponParent;
 
+
+    private void Start()
+    {
+        weaponParent = GetComponentInChildren<WeaponParent>();
+    }
     private void Update()
     {
         moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
@@ -22,11 +29,20 @@ public class MainCharacter : MonoBehaviour
         {
             GameplayEvents.eventGameOver.Invoke();
             eventGameOverIsInvoked = true;
-        } 
+        }
+        moveVelocity = moveInput * speed;    
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            this.Attack();
     }
 
     private void FixedUpdate()
     {
         rigidbody2D.MovePosition(rigidbody2D.position + moveVelocity * Time.fixedDeltaTime);
+    }
+
+    private void Attack()
+    {
+        weaponParent.Attack();
     }
 }
---
+++ Assets/_Project/Scripts/Gameplay/WeaponParent.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponParent : MonoBehaviour
+{
+    public SpriteRenderer charRender, weaponRender;
+    public Vector2 PointerPosition {  get; set; }
+    public Vector2 mousePos;
+    public Animator animator;
+    public float delay = 0.3f;
+
+    public Transform circleOrig;
+    public float radius;
+
+    private bool isAttack;
+
+    void Update()
+    {
+        mousePos = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        System.Console.WriteLine(mousePos);
+        Vector2 direction = (Input.mousePosition - transform.position).normalized;
+        transform.right = direction;
+
+        Vector2 scale = transform.localScale;
+        if (direction.x < 0)
+            scale.y = -1;
+        else if (direction.x > 0)
+            scale.y = 1;
+        transform.localScale = scale;
+
+        //if (transform.eulerAngles.z > 0 && transform.eulerAngles.z < 180)
+        //    weaponRender.sortingOrder = charRender.sortingOrder - 1;
+        //else
+        //    weaponRender.sortingOrder = charRender.sortingOrder + 1;
+    }
+
+    public void Attack()
+    {
+        if (isAttack) 
+            return;
+        animator.SetTrigger("Attack");
+        isAttack = true;
+        StartCoroutine(DelayAttack());
+    }
+
+    private IEnumerator DelayAttack()
+    {
+        yield return new WaitForSeconds(delay);
+        isAttack = false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Vector3 position = circleOrig == null ? Vector3.zero : circleOrig.position;
+        Gizmos.DrawWireSphere(position, radius);
+    }
+
+    public void DetectCollider()
+    {
+        foreach (Collider2D collider in Physics2D.OverlapCircleAll(circleOrig.position, radius))
+        {
+            Debug.Log(collider.name);
+        }
+    }
+}
